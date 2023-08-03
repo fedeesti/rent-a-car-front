28d244a84108d529f031301e8d332879efbf943f
github_fedeesti <fede.estigarribia@hotmail.com>
2023-08-03 11:30:20 -0300

    Add logo and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      white: '#fefefe',
+      'dark-red': '#f04e4e',
+      'light-red': 'f9b1b0',
+      'dark-green': '#1b424d',
+      'light-green': '#889da2',
+    },
   },
   plugins: [],
 };
