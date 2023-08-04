01de90a11168147c6c65e1e5c10521b76fb086b9
github_fedeesti <fede.estigarribia@hotmail.com>
2023-08-04 15:20:36 -0300

    Add styles to root and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ export default {
       'dark-green': '#1b424d',
       'light-green': '#889da2',
     },
+    gridTemplateRows: {
+      layout: 'auto 1fr auto',
+    },
+    gridTemplateColumns: {
+      'card-responsive': 'repeat(auto-fit, minmax(250px, 1fr))',
+    },
   },
   plugins: [],
 };
