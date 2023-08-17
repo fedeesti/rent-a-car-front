f6025081de319e4c1a2bdbfc4ff3b73cc050d2d9
github_fedeesti <fede.estigarribia@hotmail.com>
2023-08-17 11:53:45 -0300

    Add a custom hook, and service to create a reservation

--- src/module/reservation/hooks/useFormReservation.ts
+++ src/module/reservation/hooks/useFormReservation.ts
@@ -1,28 +1,43 @@
-import { useState } from 'react';
-
-const INITIAL_VALUES = {
-  startDate: '',
-  finishDate: '',
-  pricePerDay: '',
-  totalPrice: '',
-  paymentMethod: '',
-  statusId: '',
-  car: '',
-  client: '',
-};
+import { ChangeEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ReservationService } from '../service';
+import { Reservation } from '../entities/reservation';
+import useClients from '../../client/hooks/useClients';
+import useCars from '../../car/hooks/useCars';
+import { ReservationFormData } from '../types/reservation';
+import formDataToReservationEntity from '../mapper';
+import { INITIAL_VALUES } from '../utils/constants';
 
 function useFormReservation() {
-  const [formData, setFormData] = useState(INITIAL_VALUES);
+  const [formData, setFormData] = useState<ReservationFormData>(INITIAL_VALUES);
+  const { clients } = useClients();
+  const { cars } = useCars();
+  const navigate = useNavigate();
+  const service = new ReservationService();
 
-  const onSubmitForm = async (values: any) => {
+  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onChangeSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onSubmitForm = async (values: ReservationFormData): Promise<void> => {
     try {
-      console.log(values);
+      const reservation: Reservation = formDataToReservationEntity(values);
+      service.create(reservation);
+      navigate('/reservation');
     } catch (error) {
       console.log('Error al enviar el formulario: ', error);
     }
   };
 
-  return { formData, onSubmitForm };
+  return { formData, clients, cars, onChangeInput, onChangeSelect, onSubmitForm };
 }
 
 export default useFormReservation;
